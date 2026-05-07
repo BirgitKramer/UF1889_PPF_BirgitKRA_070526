ca3d807d4152de5a87bee85fdfb6e6f63d1dc524
BirgitKRA <birgit@gmail.com>
2026-05-07 14:03:19 +0100

    actualizar

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "version":"1.0",
     "summary":"Gestión básica de tareas asociadas a clientes",
     "category":"Productivity",
-    "author":"Rafael Martín PPF1889",
+    "author":"BirgitKRA PPF1889",
     "depends":["base","sale","contacts"],
     "data":[
         "security/ir.model.access.csv",
